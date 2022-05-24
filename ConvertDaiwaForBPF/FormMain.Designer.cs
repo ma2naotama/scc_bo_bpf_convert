63863320fa5a4ab78f1178c11d9d4e51ed17d2a7
nmatsunaga <ma2games.jp@gmail.com>
2022-05-24 18:52:05 +0900

    画面サイズ変更

--- ConvertDaiwaForBPF/FormMain.Designer.cs
+++ ConvertDaiwaForBPF/FormMain.Designer.cs
@@ -50,7 +50,7 @@
             this.textBox_Log.Multiline = true;
             this.textBox_Log.Name = "textBox_Log";
             this.textBox_Log.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-            this.textBox_Log.Size = new System.Drawing.Size(469, 158);
+            this.textBox_Log.Size = new System.Drawing.Size(647, 172);
             this.textBox_Log.TabIndex = 0;
             // 
             // textBox1
@@ -61,7 +61,7 @@
             this.textBox1.Font = new System.Drawing.Font("ＭＳ ゴシック", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
             this.textBox1.Location = new System.Drawing.Point(21, 41);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(390, 20);
+            this.textBox1.Size = new System.Drawing.Size(568, 20);
             this.textBox1.TabIndex = 3;
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             this.textBox1.DragDrop += new System.Windows.Forms.DragEventHandler(this.textBox1_DragDrop);
@@ -70,7 +70,7 @@
             // button1
             // 
             this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.button1.Location = new System.Drawing.Point(417, 43);
+            this.button1.Location = new System.Drawing.Point(595, 43);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(46, 22);
             this.button1.TabIndex = 4;
@@ -86,7 +86,7 @@
             this.textBox2.Font = new System.Drawing.Font("ＭＳ ゴシック", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
             this.textBox2.Location = new System.Drawing.Point(21, 99);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(390, 20);
+            this.textBox2.Size = new System.Drawing.Size(568, 20);
             this.textBox2.TabIndex = 5;
             this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             this.textBox2.DragDrop += new System.Windows.Forms.DragEventHandler(this.textBox2_DragDrop);
@@ -95,7 +95,7 @@
             // button2
             // 
             this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.button2.Location = new System.Drawing.Point(417, 101);
+            this.button2.Location = new System.Drawing.Point(595, 101);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(46, 22);
             this.button2.TabIndex = 6;
@@ -111,7 +111,7 @@
             this.textBox3.Font = new System.Drawing.Font("ＭＳ ゴシック", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
             this.textBox3.Location = new System.Drawing.Point(21, 159);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(390, 20);
+            this.textBox3.Size = new System.Drawing.Size(568, 20);
             this.textBox3.TabIndex = 7;
             this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
             this.textBox3.DragDrop += new System.Windows.Forms.DragEventHandler(this.textBox3_DragDrop);
@@ -120,7 +120,7 @@
             // button3
             // 
             this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.button3.Location = new System.Drawing.Point(417, 161);
+            this.button3.Location = new System.Drawing.Point(595, 161);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(46, 22);
             this.button3.TabIndex = 8;
@@ -132,7 +132,7 @@
             // 
             this.buttonConvert.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.buttonConvert.Enabled = false;
-            this.buttonConvert.Location = new System.Drawing.Point(336, 215);
+            this.buttonConvert.Location = new System.Drawing.Point(514, 215);
             this.buttonConvert.Name = "buttonConvert";
             this.buttonConvert.Size = new System.Drawing.Size(127, 54);
             this.buttonConvert.TabIndex = 9;
@@ -171,7 +171,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(488, 471);
+            this.ClientSize = new System.Drawing.Size(666, 485);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
